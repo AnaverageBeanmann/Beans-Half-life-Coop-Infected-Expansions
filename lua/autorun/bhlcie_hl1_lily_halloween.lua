b1f5e29ce2978dab5b2cb7b708b54a2d23469b27
An average Beanmann <102012974+AnaverageBeanmann@users.noreply.github.com>
2023-09-21 18:34:33 -0400

    Add files via upload

--- lua/autorun/bhlcie_hl1_lily_halloween.lua
+++ lua/autorun/bhlcie_hl1_lily_halloween.lua
@@ -16,8 +16,148 @@ if VJExists == true then
 
 	include('autorun/vj_controls.lua') -- do we need this?
 
-	local vCat = "Animals"
+	/*
+		- Updated Michael's hunt mode. If Michael is in hunt mode for about a minute or so and hasn't found anyone, he'll be told where everyone is.
+		- If Michael is stuck somewhere and can't get out on his own, he'll be teleported away to one of his start positions. [TBD]
+		- Michael now has no collision while fleeing and hiding, and won't go back into hunt mode if he's inside something. This should hopefully reduce the chances of him getting stuck on props and stuff. [TBD] - do we really need this?
+		- Reduced Michaels base hp to 100.
+		- Reduced Michaels re-hunt health increase bonus to 0.10 per flee.
+		- Michael now spawns near the portal to HERESY.
+		- Cars now give fuel.
+		- Barrels in more risky spots now generally carry more fuel, while ones in more safe spots have less.
+		- Added more items outside. [TBD]
+		- Moved the Tau Cannon in HERESY and also added more items in there.
+		- Added items to the abandoned offices.
+		- Increased the time between waves.
+		- Waves now last for three minutes instead of four.
+		- Added a zombie spawn to the furnace room. [TBD]
+		- Added another music set. You can change between it and the FAITH pack with 'hl1_coop_sv_bhlcie_lilyhalloween_music' before the match starts.
+		- Made the FAITH music quieter. [TBD]
+		- Froze the table and boxes blocking the door to a specific abandoned offices room.
+		- Fixed floaters. [TBD]
+		
+		music for new pack
+		
+		intro
+		https://youtu.be/nc1YgQXuP0E (kingdom hearts halloween town calm)
+
+		waves
+		https://youtu.be/K2rwxs1gH9w (spooky scary skeletons)
+		https://youtu.be/ho9rZjlsyYY (toccata and fugue in d minor/dracula music)
+		https://youtu.be/TaV1r341wYk (ghostbusters theme)
+		https://youtu.be/RI9YjHGuSOE (kingdom hearts halloween town battle)
+		https://youtu.be/SOFCQ2bfmHw (monster mash)
+		https://youtu.be/yRRqxnjslb0 (somebody's watching me)
+		https://youtu.be/BoatSoGva_I (little dark age)
+		https://youtu.be/lVLXNgk9uJ8 (donacdum) (maybe not)
+		https://youtu.be/fhltB4DbtDo?list=PLy6lOuS_V1SqEo5An9PCYauJcPohbr4xW (the mark)
+		https://youtu.be/t-9xbgTHpLA?list=PLU4ktq2pWONtSYFFHnFZPFfSXMF5Rw0fn (peper steak)
+		https://youtu.be/KLv9PMEkvs8 (snake eyes)
+		devil's bath boys
+		https://youtu.be/iwa8ssjSajU?list=PLpoeCpd_sH6NXV6_A9CjjsfT-lGQOY6Wl (dive to the heart)
+		https://youtu.be/8jB9OJJj7TU?list=PLpoeCpd_sH6NXV6_A9CjjsfT-lGQOY6Wl (scherzo de notte)
+		deep blue?
+		
+		tf2 (rip these from files)
+		https://youtu.be/AfjqL0vaBYU?list=PL4E9ABD7FF1106BD3 (haunted fortress 2)
+		https://youtu.be/9QpmfOLaUcw?list=PL4E9ABD7FF1106BD3 (misfortune teller)
+		
+		check cry of fear, resident evil, and pizza tower soundtracks
+		 
+		holdout
+		https://youtu.be/4BkRjWGPrSQ?list=PLU4ktq2pWONtSYFFHnFZPFfSXMF5Rw0fn (fake orchestra)
+		https://youtu.be/dyOvTf2HolA?list=PLU4ktq2pWONtSYFFHnFZPFfSXMF5Rw0fn (avatar beat)
+		https://youtu.be/KLv9PMEkvs8?t=113 (snake eyes combat)
+		https://youtu.be/7xSUwK7uIxc?t=295 (destiny's force)
+		https://youtu.be/KhdlCRXX6dw?list=PLpoeCpd_sH6NXV6_A9CjjsfT-lGQOY6Wl (fragments of sorrow)
+		moonlight sonata 3rd movement?
+		
+		escape
+		https://youtu.be/dyw6kQ5O-YA?list=PLU4ktq2pWONtSYFFHnFZPFfSXMF5Rw0fn (unreasonable behavior)
+		https://youtu.be/7xSUwK7uIxc?t=463 (night of fate)
+		https://youtu.be/7xSUwK7uIxc?t=719 (forze de male)
+		https://youtu.be/ZEHsIcsjtdI (thriller)
+
+
+		-=< Main Credits >=-
+		An average Beanmann: Putting this addon together.
+		Upset: Original creator of Half-Life Coop and Half-Life Coop Infected, any and all modified or adapted code used for this addon.
+		Lily McFluffy Butt: Creator of the map used.
+		DrVrej: Creator or VJ Base, which Michael Davies uses.
+		Omi-Box: Original Rake texture used for Michael Davies.
+		Airdorf Games: Any and all Faith: The Unholy Trinity assets, including Michael's voice clips and all music from the game.
+		Comrade Blue, Lebert130, Imaya: Playtesting and giving feedback.
+
+		-=< Music Credits >=-
+		Yoko Shimomura: Any and all music from Kingdom Hearts 1.
+		quivel: Snake Eyes (https://youtu.be/KLv9PMEkvs8).
+		Ray Parker Jr.: Ghostbusters Theme.
+		Bobby "Boris" Pickett & The Crypt Kickers: Monster Mash.
+		Rockwell: Somebody's Watching Me.
+		Andrew Gold: Spooky Scary Skeletons.
+		Johann Sebastian Bach(?): Toccata and Fugue in D Minor.
+		MGMT: Little Dark Age.
+		Simon Viklund: The Mark.
+		Alias Conrad Coldwood: Any and all music from OFF.
+		Widdly 2 Diddly: Devil's Bath Boys.
+		Mike Morasky: Any and all music from Team Fortress 2.
+		Michael Jackson: Thriller.
+		Beethoven: Moonlight Sonata 3rd Movement.
+		Ronan "Mr. Sauceman" de Castel: Any and all music from Pizza Tower.
+		ROPE: Any and all music from Zombie Night Terror.
+		
+		
+		
+Michael Davies
+- Based off the enemy of the same name from Faith
+- Wanders around topside
+- If he hasn't found anyone after a minute, he will be told where everyone is
+- Will announce himself with a warning sound apon seeing a target
+- Can be fought off, but not killed
+- When he loses his health, he runs off for a bit
+- After a while of hiding, he retuns to hunt mode with increased hp
+
+The Follower
+- Silent Hill Downpour's Bogeyman with some changes
+- Very high health
+- Very slow, about as fast as a hl2 zombie, if not slower
+- Can't be killed, but will teleport away and heal before it dies
+- Will constantly follow you through the entire map
+- Sometimes teleports to HERESY if it feels like it
+
+The Shepherd
+- Arrival is marked by the church bell ringing 3-6 times
+- Father Grigori but fucked up and evil
+- Gun shoots projectiles instead of being hitscan
+- Can be killed, but will return eventually
+
+IT
+- Creature that wanders around topside, but doesn't actively attack
+- Immortal
+- If you attack it, it will enter a frenzy and begin attacking
+- It will calm and teleport away if it hits you or enough time has passed
+
+The Phantoms
+- Ghostly Combine Stalkers
+- Ambient threat in HERESY
+- Slow
+- Immortal
+- Smites you after a few seconds aslong as your next to it
+- Has no collision, so you and enemies can just walk through it
+	
+	
+	
+	
+	*/
+
+		local vCat = "BHLCIE"
 	VJ.AddNPC("Michael Davies","npc_bhlcie_michael",vCat)
+	/*
+	VJ.AddNPC("The Follower","npc_bhlcie_follower",vCat)
+	VJ.AddNPC("The Shepherd","npc_bhlcie_shepherd",vCat)
+	VJ.AddNPC("IT","npc_bhlcie_it",vCat)
+	VJ.AddNPC("Phantom","npc_bhlcie_phantom",vCat)
+	*/
 
 	VJ.AddConVar("hl1_coop_sv_bhlcie_lilyhalloween_enemies", 0, {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "This ConVar replaces the enemies on the config for the map 'GM_HL1_Lily_Halloween'. Type 'hl1_coop_sv_bhlcie_lilyhalloween_enemies_printsetups' to see all the setups.")
 	VJ.AddConVar("hl1_coop_sv_bhlcie_lilyhalloween_music", 0, {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "This ConVar replaces the music on the config for the map 'GM_HL1_Lily_Halloween'. This needs to be changed before the match starts to take effect. 'hl1_coop_sv_bhlcie_lilyhalloween_music 1' will change it to music from Faith: The Unholy Trinity, otherwise it'll play a mishmash of music I liked.")
--- lua/entities/npc_bhlcie_michael/init.lua
+++ lua/entities/npc_bhlcie_michael/init.lua
@@ -17,6 +17,8 @@ ENT.MeleeAttackDistance = 35
 ENT.MeleeAttackDamageDistance = 75
 ENT.TimeUntilMeleeAttackDamage = 0.6
 
+ENT.AnimTbl_Walk = {ACT_RUN}
+
 ENT.FootStepTimeRun = 0.25
 
 ENT.AlertSoundLevel = 100
@@ -65,14 +67,35 @@ ENT.BHLCIE_Michael_PlayedWarnSound = false
 ENT.BHLCIE_Michael_TimesFendedOff = 1
 ENT.BHLCIE_Michael_Patience = 0
 ENT.BHLCIE_Michael_LostPatient = false
+ENT.BHLCIE_Michael_Difficulty = 1
+/*
+1 - Easy
+2 - Medium
+3 - Hard
+*/
+ENT.BHLCIE_Michael_Difficulty_ChangeWarning = true
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomOnPreInitialize()
+	self.BHLCIE_Michael_Difficulty = GetConVar("hl1_coop_sv_skill"):GetInt()
+	if self.BHLCIE_Michael_Difficulty == 3 then
+		self.StartHealth = 300
+	elseif self.BHLCIE_Michael_Difficulty == 2 then
+		self.StartHealth = 200
+	end
 	self.StartHealth = self.StartHealth * player.GetCount()
 	self:SetHealth(self.StartHealth)
+	PrintMessage(4,"Michael's health is now "..self.StartHealth.."")
+	-- PrintMessage(4,"Michael's difficulty is now "..self.BHLCIE_Michael_Difficulty.."")
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomOnInitialize()
-	self.BHLCIE_Michael_Patience = CurTime() + 60
+	if self.BHLCIE_Michael_Difficulty == 3 then
+		self.BHLCIE_Michael_Patience = CurTime() + math.random(5,10)
+	elseif self.BHLCIE_Michael_Difficulty == 2 then
+		self.BHLCIE_Michael_Patience = CurTime() + 30
+	else
+		self.BHLCIE_Michael_Patience = CurTime() + 60
+	end
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomOnAcceptInput(key, activator, caller, data)
@@ -85,83 +108,147 @@ end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomOnThink_AIEnabled()
 
-	
-	-- print("Michael's patience is "..self.BHLCIE_Michael_Patience.."")
-	-- PrintMessage(4,"Michael's patience is "..self.BHLCIE_Michael_Patience.."")
-
-	if self.BHLCIE_Michael_CurrentMode == 0 then
-		if !self.BHLCIE_Michael_PlayedWarnSound && self:GetEnemy() != nil && self:Visible(self:GetEnemy()) then
+	if self.BHLCIE_Michael_CurrentMode == 0 then -- we're hunting!
+		if !self.BHLCIE_Michael_PlayedWarnSound && self:GetEnemy() != nil && self:Visible(self:GetEnemy()) then -- check if we haven't played our warning sound and we have a valid and visible enemy
+			-- play the warning sound
 			VJ_EmitSound(self,self.SoundTbl_Warn,100,100)
 			self.BHLCIE_Michael_PlayedWarnSound = true
 		end
-		if self.BHLCIE_Michael_Patience < CurTime() && !self.BHLCIE_Michael_LostPatient then
+		if self.BHLCIE_Michael_Patience < CurTime() && !self.BHLCIE_Michael_LostPatient then -- if our patience counter has hit 0 and we haven't lost our patience..
+			-- lose patience. michael is now omnipotenent
 			self.BHLCIE_Michael_LostPatient = true
 			self.FindEnemy_CanSeeThroughWalls = true
 			self.FindEnemy_UseSphere = true
-			print("Michael has lost his patience!")
 		end
 	end
 
-	if self.BHLCIE_Michael_CurrentMode == 1 then
-
-		if self:GetEnemy() != nil then
+	if self.BHLCIE_Michael_CurrentMode == 1 then -- we're fleeing
+		if self:GetEnemy() != nil then -- check if we have no valid enemy
 			local enemydist = self:GetPos():Distance(self:GetEnemy():GetPos()) -- distance check
-			if !self:Visible(self:GetEnemy()) && enemydist >= 250 then
+			if !self:Visible(self:GetEnemy()) && enemydist >= 250 or !(self:GetEnemy():GetForward():Dot((self:GetPos() -self:GetEnemy():GetPos()):GetNormalized()) > math.cos(math.rad(60))) then -- if we can't see our current enemy and they're far enough..
+				-- go into hiding
 				self:SetMaterial("hud/killicons/default")
 				self:DrawShadow(false)
 				self:RemoveAllDecals()
-				self.BHLCIE_Michael_HideTime = CurTime() + math.random(30,180)
+				if self.BHLCIE_Michael_Difficulty == 3 then
+					self.BHLCIE_Michael_HideTime = CurTime() + math.random(10,60)
+				elseif self.BHLCIE_Michael_Difficulty == 2 then
+					self.BHLCIE_Michael_HideTime = CurTime() + math.random(20,120)
+				else
+					self.BHLCIE_Michael_HideTime = CurTime() + math.random(30,180)
+				end
 				self.BHLCIE_Michael_CurrentMode = 2
 				self.HasFootStepSound = false
+				self.FindEnemy_CanSeeThroughWalls = true
+				self.FindEnemy_UseSphere = true
 			end
 		end
-		if self:GetEnemy() == nil then
+		if self:GetEnemy() == nil then -- if we have no valid enemy then..
+			-- go into hiding
 			self:SetMaterial("hud/killicons/default")
 			self:DrawShadow(false)
 			self:RemoveAllDecals()
-			self.BHLCIE_Michael_HideTime = CurTime() + math.random(30,180)
+			if self.BHLCIE_Michael_Difficulty == 3 then
+				self.BHLCIE_Michael_HideTime = CurTime() + math.random(10,60)
+			elseif self.BHLCIE_Michael_Difficulty == 2 then
+				self.BHLCIE_Michael_HideTime = CurTime() + math.random(20,120)
+			else
+				self.BHLCIE_Michael_HideTime = CurTime() + math.random(30,180)
+			end
 			self.BHLCIE_Michael_CurrentMode = 2
-			self.BHLCIE_Michael_Patience = CurTime() + 60
 			self.HasFootStepSound = false
 		end
 
 	end
 
-	if self.BHLCIE_Michael_CurrentMode == 2 then
-		if self.BHLCIE_Michael_HideTime < CurTime() then
-			local enemydist = self:GetPos():Distance(self:GetEnemy():GetPos()) -- distance check
-			if self:GetEnemy() != nil && (!self:Visible(self:GetEnemy()) && enemydist >= 1000 or !(self:GetEnemy():GetForward():Dot((self:GetPos() -self:GetEnemy():GetPos()):GetNormalized()) > math.cos(math.rad(60)))) then
+	if self.BHLCIE_Michael_CurrentMode == 2 then -- we're hiding
 
-				self.BHLCIE_Michael_LostPatient = false
-				self.FindEnemy_CanSeeThroughWalls = false
-				self.FindEnemy_UseSphere = false
-				self:SetMaterial(nil)
-				self:DrawShadow(true)
-				self:RemoveAllDecals()
-				self.BHLCIE_Michael_CurrentMode = 0
-				self.HasFootStepSound = true
-				self.Behavior = VJ_BEHAVIOR_AGGRESSIVE
-				self.BHLCIE_Michael_PlayedWarnSound = false
-				self.GodMode = false
-				
-				self:SetRenderFX(0)
-				local thefunny = Color(255, 255, 255, 255)
-				self:SetColor(thefunny)
+		if self.BHLCIE_Michael_HideTime < CurTime() then -- if our hide time check hits 0 then..
 
-				self:SetHealth(self.StartHealth * self.BHLCIE_Michael_TimesFendedOff)
+			if IsValid(self:GetEnemy()) then -- if we have a valid enemy..
+
+				local enemydist = self:GetPos():Distance(self:GetEnemy():GetPos()) -- distance check
+
+				if
+					self:GetEnemy() != nil -- another valid enemy check?
+						&&
+					(
+						!self:Visible(self:GetEnemy())
+							&&
+						(
+							enemydist >= 1000
+						or
+							!(self:GetEnemy():GetForward():Dot((self:GetPos() -self:GetEnemy():GetPos()):GetNormalized()) > math.cos(math.rad(60)))
+						)
+					)
+					-- if our current enemy isn't visislbe and is far enough, or they're not looking at us, then..
+
+				then
+
+					-- go on the hunt!
+
+					self.GodMode = false
+					self.BHLCIE_Michael_CurrentMode = 0
+					self.BHLCIE_Michael_LostPatient = false
+					if self.BHLCIE_Michael_Difficulty == 3 then
+						self.BHLCIE_Michael_Patience = CurTime() + math.random(5,10)
+					elseif self.BHLCIE_Michael_Difficulty == 2 then
+						self.BHLCIE_Michael_Patience = CurTime() + 30
+					else
+						self.BHLCIE_Michael_Patience = CurTime() + 60
+					end
+					self.FindEnemy_CanSeeThroughWalls = false
+					self.FindEnemy_UseSphere = false
+					self.Behavior = VJ_BEHAVIOR_AGGRESSIVE
+					self.BHLCIE_Michael_PlayedWarnSound = false
+					self.HasFootStepSound = true
+
+					self:SetMaterial(nil)
+					self:DrawShadow(true)
+					self:RemoveAllDecals()
+					self:SetRenderFX(0)
+					local thefunny = Color(255, 255, 255, 255)
+					self:SetColor(thefunny)
+
+					self:SetHealth(self.StartHealth * self.BHLCIE_Michael_TimesFendedOff)
+					PrintMessage(4,"Michael's health is now "..self:Health().."")
+				end
 
 			else
-				self.BHLCIE_Michael_HideTime = CurTime() + 5
+
+				-- we didn't meet the requirements, keep hiding
+				self.BHLCIE_Michael_HideTime = CurTime() + 3
+
 			end
+
 		end
+
+	end
+
+	if
+		(
+			self.BHLCIE_Michael_Difficulty == 1 && GetConVar("hl1_coop_sv_skill"):GetInt() != 1
+		or
+			self.BHLCIE_Michael_Difficulty == 2 && GetConVar("hl1_coop_sv_skill"):GetInt() != 2
+		or
+			self.BHLCIE_Michael_Difficulty == 3 && GetConVar("hl1_coop_sv_skill"):GetInt() != 3
+		)
+		&&
+		self.BHLCIE_Michael_Difficulty_ChangeWarning
+	then
+		self.BHLCIE_Michael_Difficulty_ChangeWarning = false
+		PrintMessage(4,"Please restart the match/map to get the full effect of changing the difficulty!")
 	end
 
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomOnTakeDamage_BeforeDamage(dmginfo, hitgroup)
 
-	if (self:Health() - dmginfo:GetDamage()) <= 0 && self.Dead == false then
+	if (self:Health() - dmginfo:GetDamage()) <= 0 && self.Dead == false then -- if we take lethal damage then..
 
+		dmginfo:ScaleDamage(0.001) -- to avoid him actually dying
+
+		-- flee!
 		VJ_EmitSound(self,self.SoundTbl_Flee,100,100)
 
 		self:SetHealth(self.StartHealth)
@@ -169,7 +256,13 @@ function ENT:CustomOnTakeDamage_BeforeDamage(dmginfo, hitgroup)
 		timer.Simple(0.15, function() if IsValid(self) then
 			self:SetHealth(self.StartHealth)
 		end end)
-		self.BHLCIE_Michael_TimesFendedOff = self.BHLCIE_Michael_TimesFendedOff + 0.10
+		if self.BHLCIE_Michael_Difficulty == 3 then
+			self.BHLCIE_Michael_TimesFendedOff = self.BHLCIE_Michael_TimesFendedOff + 0.45
+		elseif self.BHLCIE_Michael_Difficulty == 2 then
+			self.BHLCIE_Michael_TimesFendedOff = self.BHLCIE_Michael_TimesFendedOff + 0.30
+		else
+			self.BHLCIE_Michael_TimesFendedOff = self.BHLCIE_Michael_TimesFendedOff + 0.15
+		end
 
 		self.BHLCIE_Michael_CurrentMode = 1
 		self.Behavior = VJ_BEHAVIOR_PASSIVE
@@ -178,7 +271,7 @@ function ENT:CustomOnTakeDamage_BeforeDamage(dmginfo, hitgroup)
 		self:SetRenderFX(16)
 		local thefunny = Color(200, 200, 200, 200)
 		self:SetColor(thefunny)
-			self:SetMaterial("models/wireframe")
+		self:SetMaterial("models/wireframe")
 
 	end
 
